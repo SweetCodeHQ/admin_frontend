442950cc6c3a277404713375d55144653f8bdf9e
Robert Heath <robert.b.heathii@gmail.com>
2022-08-29 14:47:42 -0500

    Remove extraneous files, update EntityDash

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,6 @@
 export { default as Loader } from "./Loader";
 export { default as Footer } from "./Footer";
-export { default as Header } from "./Header";
 export { default as Navbar } from "./Navbar";
 export { default as Welcome } from "./Welcome";
 export { default as Dashboard } from "./Dashboard";
-export { default as Services } from "./Services";
-export { default as Entities } from "./Entities";
+export { default as EntityDashboard } from "./EntityDashboard";
