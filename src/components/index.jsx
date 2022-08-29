7ba9fde9063b40e4de45055ad8799b53c4cf6eeb
Robert Heath <robert.b.heathii@gmail.com>
2022-08-29 07:53:46 -0500

    Remove extra files

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,3 +5,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Welcome } from "./Welcome";
 export { default as Dashboard } from "./Dashboard";
 export { default as Services } from "./Services";
+export { default as Entities } from "./Entities";
