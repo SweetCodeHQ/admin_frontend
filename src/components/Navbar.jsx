46aa20da9dcb223596379abbc65815863394b037
Robert Heath <robert.b.heathii@gmail.com>
2022-10-30 08:25:04 -0500

    Pull Cart into a separate component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,17 @@
 import { useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
-import { CartContextProvider } from "../context/CartContext";
+import { Cart } from "../components";
 
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import { BsCart4 } from "react-icons/bs";
-import { MdDeleteForever } from "react-icons/md";
 
 import logo from "../../images/black_white_logo.png";
 
-const CartItem = ({ title, classProps }) => {
-  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
-};
-
 const Navbar = () => {
   const [toggleCart, setToggleCart] = useState(false);
-  const { handleSignOut, googleUser } = useContext(UserContext);
 
-  const handleSubmitTopics = () => {
-    {
-      /* .map through cartTopics, send email, set cartTopics to [], change submitted? attribute to true */
-    }
-  };
+  const { handleSignOut, googleUser } = useContext(UserContext);
 
   return (
     <nav className="w-full gradient-bg-purple-welcome fixed">
@@ -47,28 +37,7 @@ const Navbar = () => {
             </button>
           </>
         )}
-        {toggleCart && (
-          <>
-            <CartContextProvider>
-              <ul className="z-10 fixed top-0 -right-2 p-3 w-[75vw] md:w-[35vw] h-screen shadow-2xl list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
-                <AiOutlineClose
-                  fontSize={28}
-                  className="text-white cursor-pointer mb-5"
-                  onClick={() => setToggleCart(false)}
-                />
-                <li className="flex items-center">
-                  <MdDeleteForever
-                    fontSize={20}
-                    className="text-white font-bold mr-10"
-                  />
-                  Topic Text
-                </li>
-                <li onClick={() => setCartTopics([])}>Clear Cart</li>
-                <li onClick={() => handleSubmitTopics}>Send All Topics</li>
-              </ul>
-            </CartContextProvider>
-          </>
-        )}
+        {toggleCart && <Cart setToggleCart={setToggleCart} />}
       </div>
     </nav>
   );
