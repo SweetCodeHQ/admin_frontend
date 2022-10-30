2a864712269f0120330a43d1aaf3dc4206cee5fe
Robert Heath <robert.b.heathii@gmail.com>
2022-10-30 08:22:52 -0500

    Add CartContextProvider to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Navbar, Welcome, Footer, Loader, Dashboard } from "./components";
-import { UserContext, UserContextProvider } from "./context/UserContext";
+import { UserContextProvider } from "./context/UserContext";
+
+import { CartContextProvider } from "./context/CartContext";
+
 import { useEffect, useState, useContext } from "react";
 import { useQuery, gql } from "@apollo/client";
 
 const App = () => {
-  const user = useContext(UserContext);
-
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-purple-welcome">
-        <UserContextProvider user={user}>
-          <Navbar />
-          <Dashboard />
+        <UserContextProvider>
+          <CartContextProvider>
+            <Navbar />
+            <Dashboard />
+          </CartContextProvider>
         </UserContextProvider>
         <Footer />
       </div>
