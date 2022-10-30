88e281421d315a376ef06e2853002d50e0745547
Robert Heath <robert.b.heathii@gmail.com>
2022-10-30 08:24:22 -0500

    Create/ update cartcontext with remove item

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,20 +4,34 @@ export const CartContext = React.createContext();
 
 export const CartContextProvider = ({ children }) => {
   const [cartTopics, setCartTopics] = useState([]);
-  console.log(cartTopics);
+
   const handleAddToCart = topic => {
     setCartTopics(prev => [...prev, topic]);
   };
 
-  const handleTopicEmail = () => {
-    const url = `https://megaphone-api.herokuapp.com/email?topic_id=${topicFormData.id}`;
+  const handleClearCart = () => {
+    setCartTopics(prev => []);
+  };
+
+  const handleRemoveFromCart = index => {
+    setCartTopics(prev => prev.filter((topic, i) => i !== index));
+  };
+
+  const handleTopicEmail = topicId => {
+    const url = `https://megaphone-api.herokuapp.com/email?topic_id=${topicId}`;
 
     fetch(url, { method: "POST" }).then(error => console.log(error));
   };
 
   return (
     <CartContext.Provider
-      value={{ cartTopics, handleAddToCart, handleTopicEmail }}
+      value={{
+        cartTopics,
+        handleAddToCart,
+        handleTopicEmail,
+        handleClearCart,
+        handleRemoveFromCart
+      }}
     >
       {children}
     </CartContext.Provider>
