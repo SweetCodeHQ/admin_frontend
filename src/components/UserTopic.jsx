0d2bd846403eb3da042e32e317440511669affe3
Robert Heath <robert.b.heathii@gmail.com>
2022-10-30 08:31:45 -0500

    Change variable name to allow for additional clicked? attributes

--- src/components/UserTopic.jsx
+++ src/components/UserTopic.jsx
@@ -97,7 +97,7 @@ const UserTopic = ({ topic, refetch, id }) => {
               type="button"
               className="text-blue-300 mr-3 border-[1px] border-[#3d4f7c] rounded-full cursor-pointer transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105 hover:text-purple-500"
               onClick={() => {
-                setClicked(false);
+                setClickedEdit(false);
               }}
             >
               <HiOutlineX />
@@ -145,7 +145,7 @@ const UserTopic = ({ topic, refetch, id }) => {
           <button
             className="text-blue-300 mr-3 rounded-full cursor-pointer transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105 hover:text-purple-500"
             type="button"
-            onClick={() => setClicked(true)}
+            onClick={() => setClickedEdit(true)}
           >
             <HiPencilAlt />
           </button>
