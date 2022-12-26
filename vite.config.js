57e60f1057c4ed2d3a1698a05087cb47343d5642
Robert Heath <robert.b.heathii@gmail.com>
2022-12-26 12:39:23 -0700

    Remove commented code in config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import "dotenv/config";
-// https://vitejs.dev/config/
+
 export default defineConfig({
   define: {
     "process.env": process.env
