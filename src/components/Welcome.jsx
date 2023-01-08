3a02aaba2b5d92a1bc37cca81cbbae86d83bcd12
Robert Heath <robert.b.heathii@gmail.com>
2023-01-08 11:37:00 -0600

    Update landing page with image and reposition text

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,7 @@ const commonStyles =
 
 const Welcome = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center h-screen">
       <h1 className="text-3xl sm:text-5xl text-white text-center text-bold py-1">
         Empowered <br /> Practitioner Marketing
       </h1>
@@ -11,14 +11,11 @@ const Welcome = () => {
         Get professionals talking about your company through strategically
         selected topics and blog posts written by the experts at Fixate.
       </p>
-      <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-        <div className={`rounded-tl-2xl ${commonStyles}`}>Marketing</div>
-        <div className={commonStyles}>AI Powered</div>
-        <div className={`rounded-tr-2xl ${commonStyles}`}>Fun</div>
-        <div className={`rounded-bl-2xl ${commonStyles}`}>Organic</div>
-        <div className={commonStyles}>Easy</div>
-        <div className={`rounded-br-2xl ${commonStyles}`}>Intuitive</div>
-      </div>
+      {/*<div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+        <div className={`rounded-l-2xl ${commonStyles}`}>AI Powered</div>
+        <div className={commonStyles}> Organic</div>
+        <div className={`rounded-r-2xl ${commonStyles}`}>Intuitive</div>
+      </div>*/}
     </div>
   );
 };
