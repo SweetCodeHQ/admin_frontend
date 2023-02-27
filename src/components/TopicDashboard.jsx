8a1c71a2b5d37701f2fe8e91b42bbb042ce629b7
Robert Heath <robert.b.heathii@gmail.com>
2023-02-27 09:07:54 -0600

    implement topic filter icon and state

--- src/components/TopicDashboard.jsx
+++ src/components/TopicDashboard.jsx
@@ -6,9 +6,14 @@ import {
 } from "react-icons/bs";
 import { gql, useQuery, useMutation } from "@apollo/client";
 
-import { Loader, TopicRow, UserTopic, TopicInputForm } from "../components";
+import {
+  Loader,
+  TopicRow,
+  UserTopic,
+  TopicInputForm,
+  TopicFilter
+} from "../components";
 import { ImBullhorn } from "react-icons/im";
-import { BiFilter } from "react-icons/bi";
 import { INDUSTRIES } from "../constants/industries";
 
 const GET_PAGINATED_TOPICS = gql`
@@ -111,24 +116,6 @@ export const IndustrySwitch = ({ toggleUseIndustry, setToggleUseIndustry }) => {
     setToggleUseIndustry(prev => !prev);
   };
 
-  // const [userUpdateData, { loading, mutationError }] = useMutation(
-  //   UPDATE_ADMIN_STATUS,
-  //   {
-  //     onCompleted: data => console.log(data),
-  //     onError: error => console.log(error)
-  //   }
-  // );
-  //
-  // const handleEnableClick = () => {
-  //   var newEnabled = !isEnabled;
-  //   setIsEnabled(newEnabled);
-  //   flipStatus(newEnabled);
-  // };
-  //
-  // useEffect(() => {
-  //   setIsEnabled(isOn);
-  // }, []);
-
   return (
     <>
       <div
@@ -177,9 +164,12 @@ const TopicDashboard = ({ megaphoneUserInfo, refetchUser }) => {
 
   const [freshTopics, setFreshTopics] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const [userTopics, setUserTopics] = useState(megaphoneUserInfo?.topics);
   const [userTopicsConnection, setUserTopicsConnection] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [toggleUseIndustry, setToggleUseIndustry] = useState(true);
+  const [filterTopicsBy, setFilterTopicsBy] = useState("NONE");
 
   useEffect(() => {
     userTopicsConnection.pageInfo?.hasPreviousPage ? null : setCurrentPage(1);
@@ -542,8 +532,30 @@ const TopicDashboard = ({ megaphoneUserInfo, refetchUser }) => {
               <h3 className="text-white text-3xl font-bold my-2 pr-5">
                 My Saved Topics
               </h3>
-              {/*<div className="text-4xl bg-[#3A1F5C] text-white rounded-full p-2 hover:cursor-pointer">
-                <BiFilter />
+              <TopicFilter
+                setFilterTopicsBy={setFilterTopicsBy}
+                filterTopicsBy={filterTopicsBy}
+              />
+              {/*<div className="flex justify-center">
+                <div>
+                  <div className="dropdown relative">
+                    <button
+                      className="dropdown-toggle bg-[#2D104F] p-1 text-5xl text-white rounded-full shadow-md hover:bg-white hover:text-[#2D104F] hover:shadow-lg focus:bg-white focus:text-[#2D104F] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-white active:shadow-lg active:text-[#2D104F] transition duration-150 ease-in-out flex items-center"
+                      type="button"
+                      id="dropdownMenuButton2"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <BiFilter />
+                    </button>
+                    <ul
+                      className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none bg-gray-800"
+                      aria-labelledby="dropdownMenuButton2"
+                    >
+                      <h1>Thing</h1>
+                    </ul>
+                  </div>
+                </div>
               </div>*/}
             </div>
             <div className="bg-[#3A1F5C] rounded-xl mt-2">
