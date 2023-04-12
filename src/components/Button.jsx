faec4256aa90af04ab4aedff385ba2fdc3e3ced2
Robert Heath <robert.b.heathii@gmail.com>
2023-04-12 17:15:26 -0500

    Create reusable button

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ handleClick, text, customStyles }) => {
+  const defaultStyles =
+    "bg-white py-2 px-7 mx-4 rounded-full cursor-pointer font-bold text-[#2D104F] transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105";
+
+  const styles = customStyles || defaultStyles;
+
+  return (
+    <button className={styles} type="button" onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
