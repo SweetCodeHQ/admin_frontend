6531c94fd7ca0aeb93a1006effdb6ee4c34491b6
Robert Heath <robert.b.heathii@gmail.com>
2023-04-12 17:18:11 -0500

    Use reusable button and push state to lowest level

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,14 @@
 import { useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { CartContext } from "../context/CartContext";
-import { Cart } from "../components";
+import { Cart, Button, CartIcon } from "../components";
 
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
-import { BsCart4 } from "react-icons/bs";
 
 import logo from "../../images/black_white_logo.png";
 
 const Navbar = () => {
-  const [toggleCart, setToggleCart] = useState(false);
-
   const { handleSignOut, googleUser, megaphoneUserInfo } = useContext(
     UserContext
   );
@@ -23,29 +20,10 @@ const Navbar = () => {
       <div className="w-full flex justify-between items-center p-4">
         {googleUser && (
           <>
-            <button
-              className="bg-white py-2 px-7 mx-4 rounded-full cursor-pointer font-bold text-[#2D104F] transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105"
-              type="button"
-              onClick={e => handleSignOut(e)}
-            >
-              Logout
-            </button>
-            {!megaphoneUserInfo?.isAdmin && (
-              <div className="flex">
-                <BsCart4
-                  className="text-3xl cursor-pointer text-white"
-                  onClick={() => setToggleCart(true)}
-                />
-                {cartTopics.length !== 0 && (
-                  <h4 className="text-purple-800 rounded-full text-base font-bold pt-1 pl-2 pr-2 pb-1 -translate-y-4 bg-yellow-400">
-                    {cartTopics.length}
-                  </h4>
-                )}
-              </div>
-            )}
+            <Button text={"Logout"} handleClick={handleSignOut} />
+            <CartIcon />
           </>
         )}
-        {toggleCart && <Cart setToggleCart={setToggleCart} />}
       </div>
     </nav>
   );
