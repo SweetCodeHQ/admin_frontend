ef1bfcc1824aa3b581c9ed1e78ced015d09f8aa1
Robert Heath <robert.b.heathii@gmail.com>
2023-05-04 10:53:50 -0500

    Change syntax for import statement

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,7 +2,9 @@ export { default as Loader } from "./Loader";
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export { default as Welcome } from "./Welcome";
+
 export { default as Input } from "./Input";
+export { default as Button } from "./Button";
 
 export { default as Dashboard } from "./Dashboard";
 export { default as AdminDashboards } from "./AdminDashboards";
@@ -16,8 +18,6 @@ export { default as UserTopic } from "./UserTopic";
 export { default as UserTopicModal } from "./UserTopicModal";
 export { default as Abstract } from "./Abstract";
 export { default as NoAbstract } from "./NoAbstract";
-export { default as EditTopicMenu } from "./EditTopicMenu";
-export { default as TopicAbstractMenu } from "./TopicAbstractMenu";
 export { default as TopicCartIcon } from "./TopicCartIcon";
 
 export { default as TopicInputForm } from "./TopicInputForm";
@@ -29,7 +29,6 @@ export { default as LaunchCartModalButton } from "./LaunchCartModalButton";
 
 export { default as IndustryModal } from "./IndustryModal";
 
-export { default as Button } from "./Button";
 export { default as GoogleLoginButton } from "./GoogleLoginButton";
 export { default as ExportButton } from "./ExportButton";
 export { default as ExportAlert } from "./ExportAlert";
