c54fbf9db6979099374ef9df6d423ef47cdf5132
Robert Heath <robert.b.heathii@gmail.com>
2023-05-13 09:27:03 -0500

    add ? to allow for cart icon to be reused

--- src/components/TopicCartIcon.jsx
+++ src/components/TopicCartIcon.jsx
@@ -12,14 +12,14 @@ const TopicCartIcon = ({ topic }) => {
 
   return (
     <>
-      {cartIds?.includes(topic.id) ? (
+      {cartIds?.includes(topic?.id) ? (
         <button className="text-blue-300 rounded-full text-2xl">
           <BsCartCheckFill />
         </button>
       ) : (
         <button
           type="button"
-          className={`text-blue-300 text-2xl rounded-full cursor-pointer transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105 ${topic.submitted &&
+          className={`text-blue-300 text-2xl rounded-full cursor-pointer transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105 ${topic?.submitted &&
             "hidden"}`}
           onClick={e => handleAddToCart(topic)}
         >
