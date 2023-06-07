4ea68e8f8f2a85de02614000cfc3da2d222a19d2
Robert Heath <robert.b.heathii@gmail.com>
2023-06-07 10:26:49 -0500

    Create a reusable expandable text area

---
+++ src/components/ExpandableTextArea.jsx
@@ -0,0 +1,32 @@
+import { useRef, useEffect } from "react";
+
+const ExpandableTextArea = ({
+  handleChange,
+  placeholder,
+  name,
+  defaultValue,
+  textBoxOpener,
+  formData
+}) => {
+  const textRef = useRef(null);
+  useEffect(() => {
+    if (textRef && textRef.current) {
+      textRef.current.style.height = "auto";
+      textRef.current.style.height = `${textRef.current.scrollHeight + 2}px`;
+    }
+  }, [textRef, formData, textBoxOpener]);
+
+  return (
+    <textarea
+      ref={textRef}
+      placeholder={placeholder}
+      name={name}
+      type="text"
+      defaultValue={defaultValue}
+      onChange={e => handleChange(e, name)}
+      className={`mt-1 w-full rounded-lg outline-none text-white bg-[#4E376A]/75 placeholder-gray-400 hover:border-violet-500 text-sm shadow-inner shadow-lg resize-none max-h-[250px]`}
+    />
+  );
+};
+
+export default ExpandableTextArea;
