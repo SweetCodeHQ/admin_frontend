188babae3c7776cae793c94cf87255b0945353c2
Robert Heath <robert.b.heathii@gmail.com>
2023-06-26 09:29:48 -0500

    Remove error by correctly using className

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,8 +1,8 @@
 const Tooltip = ({ text, children }) => {
   return (
-    <div class="group relative flex">
+    <div className="group relative flex">
       {children}
-      <span class="absolute bottom-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 w-[100px] text-center">
+      <span className="absolute bottom-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 w-[100px] text-center">
         {text}
       </span>
     </div>
