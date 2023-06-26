c94760c7e96970e5a0bc61033c63e0fbce2517f3
Robert Heath <robert.b.heathii@gmail.com>
2023-06-26 09:23:05 -0500

    Allows content assignment for topics in the cart

---
+++ src/components/CartTopicContentType.jsx
@@ -0,0 +1,64 @@
+import { useContext } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { FlyoutMenu } from "../components";
+import { CartContext } from "../context";
+import { CONTENT_TYPES } from "../constants/contentTypes";
+
+const UPDATE_TOPIC_TYPE = gql`
+  mutation UpdateType($id: ID!, $contentType: Int!) {
+    updateTopic(input: { id: $id, contentType: $contentType }) {
+      id
+      text
+      contentType
+    }
+  }
+`;
+
+const CartTopicContentType = ({ topicId, contentType, topicIndex }) => {
+  const { updateCartTopic } = useContext(CartContext);
+
+  const handleChangeContentType = async typeIndex => {
+    const type = typeIndex + 1;
+    try {
+      const promise = await updateTopicType(topicId, type);
+      updateCartTopic(topicIndex, type);
+    } catch (error) {
+      console.log(error);
+      alert("Sorry, I goofed! Please try again.");
+    }
+  };
+
+  const updateTopicType = async (id, type) => {
+    let promise;
+    const input = { contentType: type, id: id };
+    promise = await topicUpdateTypeData({ variables: input });
+    return promise;
+  };
+
+  const [
+    topicUpdateTypeData,
+    { loading: updateLoading, error: updateError }
+  ] = useMutation(UPDATE_TOPIC_TYPE, {
+    onError: error => console.log(error),
+    onCompleted: data => console.log(data)
+  });
+
+  return (
+    <div className="flex font-bold justify-between">
+      <FlyoutMenu
+        options={CONTENT_TYPES}
+        menuState={CONTENT_TYPES[contentType - 1]?.name.toUpperCase()}
+        handleState={handleChangeContentType}
+        menuName="Content Type"
+      />
+      {contentType === 0 ? null : (
+        <h4>
+          {CONTENT_TYPES[contentType - 1].credits}{" "}
+          {CONTENT_TYPES[contentType - 1].credits > 1 ? "Units" : "Unit"}
+        </h4>
+      )}
+    </div>
+  );
+};
+
+export default CartTopicContentType;
