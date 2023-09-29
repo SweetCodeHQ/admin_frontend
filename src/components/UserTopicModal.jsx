6e5ef576792a2e171ef8b95d1206b6fb1d242c51
Robert Heath <robert.b.heathii@gmail.com>
2023-09-29 09:57:32 -0500

    Change arrow to x

--- src/components/UserTopicModal.jsx
+++ src/components/UserTopicModal.jsx
@@ -275,7 +275,7 @@ const UserTopicModal = ({ open, setOpen, topic, refetchTopic }) => {
                       onClick={handleCloseModal}
                       className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-[#4E376A]/75 transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105 cursor-pointer focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                      <ArrowUturnLeftIcon
+                      <XMarkIcon
                         className="h-6 w-6 text-blue-300"
                         aria-hidden="true"
                       />
