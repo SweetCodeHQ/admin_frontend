f3dc1f5e9a50cd57974503330de9ac40507e0238
Robert Heath <robert.b.heathii@gmail.com>
2023-09-29 09:44:58 -0500

    Remove translation from button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ handleClick, text, customStyles }) => {
   const defaultStyles =
-    "bg-white py-2 px-7 mx-4 rounded-full cursor-pointer font-bold text-[#2D104F] transition delay-50 ease-in-out hover:-translate-y-1 hover:scale-105";
+    "bg-white py-2 px-7 mx-4 rounded-full cursor-pointer font-bold text-[#2D104F] transition delay-50 ease-in-out hover:scale-105";
 
   const styles = customStyles || defaultStyles;
 
