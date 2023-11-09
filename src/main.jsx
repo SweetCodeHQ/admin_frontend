a5f07bcd2addfb24ea537edad1d051b815b495d6
Robert Heath <robert.b.heathii@gmail.com>
2023-11-09 15:00:16 -0600

    Update Apollo client with authlink

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { relayStylePagination } from "@apollo/client/utilities";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+} from '@apollo/client';
+import { relayStylePagination } from '@apollo/client/utilities';
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+
+const httpLink = new HttpLink({
+  uri: `${process.env.MEGAPHONE_DB_URL}/graphql`,
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      ...operation.getContext().headers,
+    },
+  });
+
+  return forward(operation);
+});
 
 const client = new ApolloClient({
   uri: `${process.env.MEGAPHONE_DB_URL}/graphql`,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
         fields: {
-          comments: relayStylePagination()
-        }
-      }
-    }
-  })
+          comments: relayStylePagination(),
+        },
+      },
+    },
+  }),
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
