bde7b05eaafe27f83bf7a78a1e0913e4f9386fbe
Robert Heath <robert.b.heathii@gmail.com>
2023-11-09 16:13:13 -0600

    styles

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -1,20 +1,18 @@
-import { GoogleLogin } from "@react-oauth/google";
+import { GoogleLogin } from '@react-oauth/google';
 
-const GoogleLoginButton = ({ loginCallback, text }) => {
-  return (
-    <div className="p-5 w-full items-center flex flex-col">
-      <GoogleLogin
-        shape="pill"
-        text={text}
-        onSuccess={credentialResponse => {
-          loginCallback(credentialResponse);
-        }}
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
-    </div>
-  );
-};
+const GoogleLoginButton = ({ loginCallback, text }) => (
+  <div className="p-5 w-full items-center flex flex-col">
+    <GoogleLogin
+      shape="pill"
+      text={text}
+      onSuccess={(credentialResponse) => {
+        loginCallback(credentialResponse);
+      }}
+      onError={() => {
+        console.log('Login Failed');
+      }}
+    />
+  </div>
+);
 
 export default GoogleLoginButton;
